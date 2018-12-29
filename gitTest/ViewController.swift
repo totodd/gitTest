e6cfa9f19e5f76f5997f03dc27348c04235b1d7e
Tao Chen <tao@quizlingapp.com>
2018-12-30 10:33:36 +1100

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by TOTO on 30/12/18.
+//  Copyright © 2018 TOTO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
